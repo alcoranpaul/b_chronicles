d1beace3302a9072ba6c7b52d40aec6120196a08
alcoranpaul <67833935+alcoranpaul@users.noreply.github.com>
2025-04-25 02:29:11 -0500

    Implemented a GET api to retrieve bible verses.

---
+++ python/main.py
@@ -0,0 +1,6 @@
+from bible_to_json import fetch_bible_data
+from biblebooks import BibleBooks
+
+if __name__ == "__main__":
+    # Example usage
+    fetch_bible_data(BibleBooks.GENESIS, 1)     # Fetch Genesis 1
