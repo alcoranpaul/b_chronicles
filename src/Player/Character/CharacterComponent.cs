60215506bcaa4e348ff92e054558c06ea6536e99
alcoranpaul <67833935+alcoranpaul@users.noreply.github.com>
2025-05-20 02:55:35 -0500

    Create CharacterComponent.cs

---
+++ src/Player/Character/CharacterComponent.cs
@@ -0,0 +1,39 @@
+namespace Player;
+
+/// <summary>
+/// Manages a collection of unlocked characters for a user,
+/// including adding, retrieving, and updating character-related information.
+/// </summary>
+public class CharacterComponent : Component<Character>
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="CharacterComponent"/> class
+    /// and loads books from the saved JSON file if it exists.
+    /// </summary>
+    public CharacterComponent() : base("Character")
+    {
+
+    }
+
+    public struct UnlockCharacter
+    {
+        public UnlockCharacter(string name, List<string> traits)
+        {
+            Name = name;
+            Traits = traits;
+        }
+
+        public string Name { get; private set; }
+        public List<string> Traits { get; private set; }
+
+        public readonly bool IsNotValid()
+        {
+            bool isNameNotEmpty = Name == string.Empty;
+            bool isTraitsValid = Traits != null && Traits.Any(string.IsNullOrEmpty);
+
+            return isNameNotEmpty || isTraitsValid;
+        }
+
+
+    }
+}
