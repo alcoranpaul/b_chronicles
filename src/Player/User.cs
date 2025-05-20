4bcb2aa46cc21190ece52e109f14aec7bab1d56f
alcoranpaul <67833935+alcoranpaul@users.noreply.github.com>
2025-05-20 13:22:07 -0500

    Save character component and added player folder in gitignore.

--- src/Player/User.cs
+++ src/Player/User.cs
@@ -69,6 +69,7 @@ public class User
     public void End()
     {
         bookComponent.End();
+        characterComponent.End();
         UnlockManager.Instance.OnUnlockedEvent -= OnUnlockedEvent;
     }
 
