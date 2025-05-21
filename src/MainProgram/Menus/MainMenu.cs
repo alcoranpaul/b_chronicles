269971878fb1f80f36bbf2349e5113c0288a3465
alcoranpaul <67833935+alcoranpaul@users.noreply.github.com>
2025-05-21 16:14:18 -0500

    Removed usage of namespace Menu.

--- src/MainProgram/Menus/MainMenu.cs
+++ src/MainProgram/Menus/MainMenu.cs
@@ -10,19 +10,19 @@ public class MainMenu : Menu
     {
         _stateManager.ChangeState(GameStateManager.State.MainMenu);
 
-        Menu.Options readOption = new("Read the Bible", () =>
+        Options readOption = new("Read the Bible", () =>
         {
             _sessionManager.AddSession(BibleBooks.Genesis, 1, 1);
             _sessionManager.AddSession(BibleBooks.Genesis, 1, 2);
             _stateManager.ChangeState(GameStateManager.State.TypingSession);
         });
 
-        Menu.Options playerInfoOption = new("Profile", () =>
+        Options playerInfoOption = new("Profile", () =>
        {
            _stateManager.ChangeState(GameStateManager.State.Profile);
        });
 
-        Menu.Options exitOption = new("Exit", () =>
+        Options exitOption = new("Exit", () =>
         {
             LogDebug("Requested to end application");
             _stateManager.ChangeState(GameStateManager.State.End);
