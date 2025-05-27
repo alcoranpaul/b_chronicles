be7a8d56215206bf20c68218a09e411ac2506b75
alcoranpaul <67833935+alcoranpaul@users.noreply.github.com>
2025-05-26 23:20:39 -0500

    Added AutoUpdater for patches.

--- src/MainProgram/Menus/AppInfo.cs
+++ src/MainProgram/Menus/AppInfo.cs
@@ -15,12 +15,14 @@ public class AppInfo : Menu
             _stateManager.ChangeState(GameStateManager.State.MainMenu);
         });
 
+        Options option1 = new Options("Check for updates", AutoUpdater.CheckForUpdatesAsync);
+
 
         string appName = $"App: {Assembly.GetEntryAssembly()?.GetName().Name}";
         string appVersion = $"Version: {Assembly.GetEntryAssembly()?.GetName().Version}";
         string developer = $"Developed by: {Assembly.GetEntryAssembly()?.GetCustomAttribute<AssemblyCompanyAttribute>()?.Company}";
 
 
-        await Show("Application Information", [option2], shouldClearPrev: false, [appName, appVersion, developer]);
+        await Show("Application Information", [option2, option1], shouldClearPrev: false, [appName, appVersion, developer]);
     }
 }
