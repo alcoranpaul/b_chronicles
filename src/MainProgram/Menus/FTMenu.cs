cd69c272b6e9b34c0ffd8f35161f1825683a4a02
alcoranpaul <67833935+alcoranpaul@users.noreply.github.com>
2025-06-03 19:54:04 -0500

    Add an option to download genesis.

--- src/MainProgram/Menus/FTMenu.cs
+++ src/MainProgram/Menus/FTMenu.cs
@@ -12,7 +12,7 @@ public class FTMenu : Menu
     public override async Task ShowAsync()
     {
         // TODO: consider to make a user account.
-        Options option1 = new Options("Download Bible Raw Data (~30 mins)", async () =>
+        Options option1 = new Options("Download Complete Bible Raw Data (~30 mins)", async () =>
 {
     try
     {
@@ -35,6 +35,30 @@ public class FTMenu : Menu
         LogError($"Exception during Bible download: {ex}");
     }
 });
+
+        Options option2 = new Options("Download Genesis", async () =>
+       {
+           try
+           {
+               using HttpClient httpClient = new HttpClient();
+               var fetcher = new DataFetcher.BibleDataFetcher(httpClient);
+
+               bool success = await fetcher.DownloadBook(BookNames.Genesis);
+               if (success)
+               {
+                   LogDebug("Downloading Done!");
+                   _stateManager.ChangeState(GameStateManager.State.MainMenu);
+               }
+               else
+               {
+                   LogError("Failed to download Bible Genesis.");
+               }
+           }
+           catch (Exception ex)
+           {
+               LogError($"Exception during Genesis download: {ex}");
+           }
+       });
         Options exitOption = new("Exit", () =>
        {
            LogDebug("Requested to end application");
