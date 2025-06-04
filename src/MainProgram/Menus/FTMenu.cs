23f2ec833629bae2e50761ed9b0e346e6c49bd2f
alcoranpaul <67833935+alcoranpaul@users.noreply.github.com>
2025-06-03 19:56:18 -0500

    Added documentation and log info.

--- src/MainProgram/Menus/FTMenu.cs
+++ src/MainProgram/Menus/FTMenu.cs
@@ -9,32 +9,40 @@ namespace main;
 /// </summary>
 public class FTMenu : Menu
 {
+    /// <summary>
+    /// Displays a menu with two options to download the Bible data.
+    /// </summary>
+    /// <remarks>
+    /// The first option downloads the complete Bible raw data. This process takes around 30 minutes.
+    /// The second option downloads only the Genesis book.
+    /// </remarks>
+    /// <returns>A Task representing the asynchronous menu operation.</returns>
     public override async Task ShowAsync()
     {
         // TODO: consider to make a user account.
         Options option1 = new Options("Download Complete Bible Raw Data (~30 mins)", async () =>
-{
-    try
-    {
-        using HttpClient httpClient = new HttpClient();
-        var fetcher = new DataFetcher.BibleDataFetcher(httpClient);
-
-        bool success = await fetcher.FetchBibleDataComplete();
-        if (success)
-        {
-            LogDebug("Downloaidng Done!");
-            _stateManager.ChangeState(GameStateManager.State.MainMenu);
-        }
-        else
-        {
-            LogError("Failed to download Bible data.");
-        }
-    }
-    catch (Exception ex)
-    {
-        LogError($"Exception during Bible download: {ex}");
-    }
-});
+            {
+                try
+                {
+                    using HttpClient httpClient = new HttpClient();
+                    var fetcher = new DataFetcher.BibleDataFetcher(httpClient);
+                    LogInfo($"Downloading Bible data...");
+                    bool success = await fetcher.FetchBibleDataComplete();
+                    if (success)
+                    {
+                        LogDebug("Downloading Done!");
+                        _stateManager.ChangeState(GameStateManager.State.MainMenu);
+                    }
+                    else
+                    {
+                        LogError("Failed to download Bible data.");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    LogError($"Exception during Bible download: {ex}");
+                }
+            });
 
         Options option2 = new Options("Download Genesis", async () =>
        {
@@ -42,7 +50,7 @@ public class FTMenu : Menu
            {
                using HttpClient httpClient = new HttpClient();
                var fetcher = new DataFetcher.BibleDataFetcher(httpClient);
-
+               LogInfo($"Downloading Genesis...");
                bool success = await fetcher.DownloadBook(BookNames.Genesis);
                if (success)
                {
